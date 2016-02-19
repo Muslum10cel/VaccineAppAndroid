78a32e683029135359f8c4bd2e83c767edd55465
muslumoncel <muslumoncel10@gmail.com>
2016-02-20 01:29:28 +0200

    added some modules

--- app/src/main/java/com/muslumoncel/usermain/UserMain.java
+++ app/src/main/java/com/muslumoncel/usermain/UserMain.java
@@ -171,9 +171,7 @@ public class UserMain extends AppCompatActivity implements NavigationView.OnNavi
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        }  else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
